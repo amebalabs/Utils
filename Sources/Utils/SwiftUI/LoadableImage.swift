c414f6fe20e830d0d384c8569bf73cfec5d9169a
Alexander Mazanov <alexandr.mazanov@gmail.com>
2019-07-24 18:18:10 -0400

    Temporally disable SwiftUI related code

--- Sources/Utils/SwiftUI/LoadableImage.swift
+++ Sources/Utils/SwiftUI/LoadableImage.swift
@@ -1,27 +1,27 @@
-import SwiftUI
-
-@available(iOS 13.0, *)
-struct LoadableImage : View {
-    @State var remoteImage: RemoteImage
-
-    var body: some View {
-        ZStack {
-            if remoteImage.image != nil {
-                Image(uiImage: remoteImage.image!)
-                    .resizable()
-                    .renderingMode(.original)
-                    .frame(width: 300, height: 169)
-                    .cornerRadius(5)
-                    .shadow(radius: 10)
-            } else {
-                Rectangle()
-                    .foregroundColor(.gray)
-                    .frame(width: 300, height: 169)
-                    .cornerRadius(5)
-                    .shadow(radius: 10)
-            }
-            }.onAppear {
-                self.remoteImage.download()
-        }
-    }
-}
+//import SwiftUI
+//
+//@available(iOS 13.0, *)
+//struct LoadableImage : View {
+//    @State var remoteImage: RemoteImage
+//
+//    var body: some View {
+//        ZStack {
+//            if remoteImage.image != nil {
+//                Image(uiImage: remoteImage.image!)
+//                    .resizable()
+//                    .renderingMode(.original)
+//                    .frame(width: 300, height: 169)
+//                    .cornerRadius(5)
+//                    .shadow(radius: 10)
+//            } else {
+//                Rectangle()
+//                    .foregroundColor(.gray)
+//                    .frame(width: 300, height: 169)
+//                    .cornerRadius(5)
+//                    .shadow(radius: 10)
+//            }
+//            }.onAppear {
+//                self.remoteImage.download()
+//        }
+//    }
+//}
--- Sources/Utils/SwiftUI/RemoteImage.swift
+++ Sources/Utils/SwiftUI/RemoteImage.swift
@@ -1,31 +1,31 @@
-import SwiftUI
-import UIKit
-import Combine
-
-@available(iOS 13.0, *)
-final class RemoteImage: BindableObject {
-    let willChange = PassthroughSubject<RemoteImage,Never>()
-    private let url: URL
-    private let placeholderImage: UIImage? = nil
-
-    var image: UIImage? = nil {
-        willSet {
-            willChange.send(self)
-        }
-    }
-
-    init(url: URL) {
-        self.url = url
-    }
-
-    public func download() {
-        DispatchQueue.global(qos: .background).async {
-            URLSession.shared.dataTask(with: self.url) { [weak self] (data, _, _) in
-                guard let strongSelf = self, let data = data else { return }
-                DispatchQueue.main.async {
-                    strongSelf.image = UIImage(data: data) ?? strongSelf.placeholderImage
-                }
-                }.resume()
-        }
-    }
-}
+//import SwiftUI
+//import UIKit
+//import Combine
+//
+//@available(iOS 13.0, *)
+//final class RemoteImage: BindableObject {
+//    let willChange = PassthroughSubject<RemoteImage,Never>()
+//    private let url: URL
+//    private let placeholderImage: UIImage? = nil
+//
+//    var image: UIImage? = nil {
+//        willSet {
+//            willChange.send(self)
+//        }
+//    }
+//
+//    init(url: URL) {
+//        self.url = url
+//    }
+//
+//    public func download() {
+//        DispatchQueue.global(qos: .background).async {
+//            URLSession.shared.dataTask(with: self.url) { [weak self] (data, _, _) in
+//                guard let strongSelf = self, let data = data else { return }
+//                DispatchQueue.main.async {
+//                    strongSelf.image = UIImage(data: data) ?? strongSelf.placeholderImage
+//                }
+//                }.resume()
+//        }
+//    }
+//}
