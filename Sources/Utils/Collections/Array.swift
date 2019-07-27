df48563119fcd10cdb2bcf61e8fbd6c38b815496
Alexander Mazanov <alexandr.mazanov@gmail.com>
2019-07-27 19:11:18 -0400

    Safe array index access

--- Sources/Utils/Collections/Array.swift
+++ Sources/Utils/Collections/Array.swift
@@ -3,5 +3,9 @@ extension Array {
         let element = self.remove(at: fromIndex)
         self.insert(element, at: toIndex)
     }
-}
 
+    subscript (safe index: Int) -> Element? {
+        guard index < count, index >= 0 else {return nil}
+        return self[index]
+    }
+}
