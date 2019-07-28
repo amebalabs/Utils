703ae11cf259b9bd1456d7d8ae78b92e1ff4f8b0
Alexander Mazanov <alexandr.mazanov@gmail.com>
2019-07-28 12:02:36 -0400

    Fix for random on String

--- Sources/Utils/String.swift
+++ Sources/Utils/String.swift
@@ -1,5 +1,5 @@
-extension String {
-    func random(length: Int = 10) -> String {
+public extension String {
+    static func random(length: Int = 10) -> String {
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         return String((0..<length).map{ _ in letters.randomElement()! })
     }
