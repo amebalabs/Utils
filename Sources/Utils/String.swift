c59588693b78a6097c80fa01ff538ecb706cf79e
Alexander Mazanov <alexandr.mazanov@gmail.com>
2019-07-28 12:00:10 -0400

    Random String

---
+++ Sources/Utils/String.swift
@@ -0,0 +1,6 @@
+extension String {
+    func random(length: Int = 10) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+}
