bdda00b5ff6116cda3cb12f9e9529a6278aeaf72
Alexander Mazanov <alexandr.mazanov@gmail.com>
2019-08-10 08:19:17 -0400

    UIViewcontroller add\remove child view controllers

---
+++ Sources/Utils/UIKit/UIView/UIView.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+@available(iOS 9.0, *)
+extension UIView {
+    public func pinToSuperview(with insets: UIEdgeInsets = .zero, edges: UIRectEdge = .all) {
+        guard let superview = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        if edges.contains(.top) {
+            topAnchor.constraint(equalTo: superview.topAnchor, constant: insets.top).isActive = true
+        }
+        if edges.contains(.bottom) {
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom).isActive = true
+        }
+        if edges.contains(.left) {
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: insets.left).isActive = true
+        }
+        if edges.contains(.right) {
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -insets.right).isActive = true
+        }
+    }
+}
--- Sources/Utils/UIKit/UIViewController/UIViewController.swift
+++ Sources/Utils/UIKit/UIViewController/UIViewController.swift
@@ -12,4 +12,21 @@ extension UIViewController {
     }
 }
 
+@available(iOS 9.0, *)
+extension UIViewController {
+    public func addChild(_ child: UIViewController, in containerView: UIView) {
+        guard containerView.isDescendant(of: view) else { return }
+        addChild(child)
+        containerView.addSubview(child.view)
+        child.view.pinToSuperview()
+        child.didMove(toParent: self)
+    }
+
+    public func removeChild(_ child: UIViewController) {
+        child.willMove(toParent: nil)
+        child.view.removeFromSuperview()
+        child.removeFromParent()
+    }
+}
+
 extension UIViewController: Reusable {}
