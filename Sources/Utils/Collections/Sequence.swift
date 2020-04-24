e7b90d07529c7758e7f1264af472cf0a1dd40d85
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-04-24 09:24:06 -0400

    Support for optionals in struct

--- Sources/Utils/Collections/Sequence.swift
+++ Sources/Utils/Collections/Sequence.swift
@@ -1,10 +1,43 @@
 public extension Sequence {
-    /// Usage: ["AAA","BBB", "CCC"].sorted(by: \.count)
-    func sorted<T:Comparable>(by attribute: KeyPath<Element, T>) -> [Element] {
-        return sorted(by: { $0[keyPath: attribute] < $1[keyPath: attribute]})
+    func sorted<T:Comparable>(by attribute: KeyPath<Element, T?>) -> [Element] {
+        return sorted(by: { (lhs, rhs) in
+            if let lhs = lhs[keyPath: attribute], let rhs = rhs[keyPath: attribute] {
+                return lhs < rhs
+            }
+
+            if lhs[keyPath: attribute] == nil && rhs[keyPath: attribute] == nil {
+                return true
+            }
+
+            if lhs[keyPath: attribute] == nil {
+                return true
+            }
+
+            if rhs[keyPath: attribute] == nil {
+                return false
+            }
+            return false
+        })
     }
-    /// Usage: ["AAA","BBB", "CCC"].sorted(by: \.count)
-    func sortedDesc<T:Comparable>(by attribute: KeyPath<Element, T>) -> [Element] {
-        return sorted(by: { $0[keyPath: attribute] > $1[keyPath: attribute]})
+    
+    func sortedDesc<T:Comparable>(by attribute: KeyPath<Element, T?>) -> [Element] {
+        return sorted(by: { (lhs, rhs) in
+            if let lhs = lhs[keyPath: attribute], let rhs = rhs[keyPath: attribute] {
+                return lhs > rhs
+            }
+
+            if lhs[keyPath: attribute] == nil && rhs[keyPath: attribute] == nil {
+                return true
+            }
+
+            if lhs[keyPath: attribute] == nil {
+                return false
+            }
+
+            if rhs[keyPath: attribute] == nil {
+                return true
+            }
+            return false
+        })
     }
 }
