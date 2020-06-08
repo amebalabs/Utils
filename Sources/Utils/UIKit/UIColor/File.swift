471339a24904dd8bafed7eec9d437f99efac29f7
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-06-08 17:15:38 -0400

    Create File.swift

---
+++ Sources/Utils/UIKit/UIColor/File.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+}
+
